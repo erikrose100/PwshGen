d472f9e5df3bc7c07be4247056b05453e92ded06
e.rose <reconroseeri@gmail.com>
2024-05-30 22:26:32 -0400

    new options + readme + new release targets

--- Template.cs
+++ Template.cs
@@ -1,26 +1,14 @@
 namespace PwshGen
 {
-    internal struct Template(string paramName, string process)
+    internal struct Template(string paramName, string process, string creatorName, DateTime createdDate)
     {
         public string requires = "#requires -version 7";
-        public string help = string.Format("<#\n.SYNOPSIS\n  <>\n\n.DESCRIPTION\n  <>\n\n.PARAMETER  <{0}>\n  <>\n\n.INPUTS\n  <>\n\n.OUTPUTS\n  <>\n\n.NOTES\n  Version:\t\t1.0\n  Author:\t\t<Name>\n  Creation Date:\t<Date>\n  Purpose/Change: Init\n\n.EXAMPLE\n  <>\n#>", paramName);
-        public string cmdletBinding = "[CmdletBinding()]\n";
-        public string scriptParams = string.Format("[CmdletBinding()]\nparam(\n\t[string]${0}\n)", paramName);
-        public string subFunction = string.Format("function FunctionName {{\n\tparam(\n\t\t[string]$FunctionParam\n\t){0}}}\n", process);
-        public string mainFunction = string.Format("function Main {{\n\tFunctionName -FunctionParam ${0}\n}}", paramName);
+        public string help = string.Format("\n<#\n.SYNOPSIS\n  <>\n\n.DESCRIPTION\n  <>\n\n.PARAMETER  <{0}>\n  <>\n\n.INPUTS\n  <>\n\n.OUTPUTS\n  <>\n\n.NOTES\n  Version:\t\t1.0\n  Author:\t\t{1}\n  Creation Date:\t{2}\n  Purpose/Change: Init\n\n.EXAMPLE\n  <>\n#>", paramName, creatorName, createdDate.ToString("d"));
+        public string cmdletBinding = "\n[CmdletBinding()]";
+        public string scriptParams = string.Format("\nparam(\n\t[string]${0})", paramName);
+        public string subFunction = string.Format("\nfunction FunctionName {{\n\tparam(\n\t\t[string]$FunctionParam\n\t){0}}}\n", process);
+        public string mainFunction = string.Format("\nfunction Main {{\n\tFunctionName -FunctionParam ${0}\n}}", paramName);
         
         public string run = "\nMain\nexit 0";
-        // public string process = "\n\n\tbegin {\n\t}\n\n\tprocess {\n\t}\n\n\tend {\n\t}";
     }
-
-    // internal class Template
-    // {
-    //     public string? requires;
-    //     public string? help;
-    //     public string? cmdletBinding;
-    //     public string? scriptParams;
-    //     public string? subFunction;
-    //     public string? mainFunction;
-    //     public string? run;
-    // }
 }
